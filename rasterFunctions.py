f1a206982004464750674db7ea02087958840f2d
gwib <gwib@users.noreply.github.com>
2020-06-17 15:52:53 +0200

    add title and save fig

--- rasterFunctions.py
+++ rasterFunctions.py
@@ -11,8 +11,13 @@ import matplotlib.pyplot as plt
 #import rasterio.warp as warp
 #import rasterio.plot as rioPlt
 import numpy as np
+import re
+import os
+from pathlib import Path
 
-filepath = '/Volumes/ElementsSE/thesisData/FCCbatch/FCC_Sigma0_HHHV_20190412.tif'
+
+#filepath = '/Volumes/ElementsSE/thesisData/FCCbatch/FCC_Sigma0_HHHV_20190412.tif'
+targetDir = '/Volumes/ElementsSE/thesisData/hist/'
 
 def readBands(filepath):
     file = rio.open(filepath)
@@ -31,7 +36,20 @@ def removeNansFromArray(inArray):
     outArray[outArray == 0] = np.nan
     return outArray
 
-def plotHistogramsForTif(filepath):
+def dateFromFilename(fn):
+    s1 = fn.split('_')[-1]
+    s2 = s1.split('.')[0]
+    return s2
+
+def plotHistogramsForTif(filepath, targetDir=''):
+    
+    if len(targetDir) < 1:
+        targetDir = os.path.dirname(filepath)+'/hist/'
+    
+    splitDate = dateFromFilename(os.path.split(filepath)[-1])
+    print(splitDate)
+    figName = targetDir+'hist_'+splitDate[3]+'.png'
+    
     HH, HV = readBands(filepath)
     
     if np.isnan(HV).all():
@@ -46,9 +64,14 @@ def plotHistogramsForTif(filepath):
         
         ax1.hist(HH)
         ax2.hist(HV)
+        
+        ax1.set_title('HH polarisation')
+        ax2.set_title('HV polarisation')
     
         ax1.get_shared_x_axes().join(ax1, ax2)
         ax1.set_xticklabels([])
         # ax2.autoscale() ## call autoscale if needed
         
     plt.show()
+    Path(targetDir).mkdir(parents=True, exist_ok=True)
+    plt.savefig(figName)
