22a3165ccbdb6762208c4d0c0b48ea92ca6d56a1
gwib <gwib@users.noreply.github.com>
2020-06-22 09:30:07 +0200

    putting things together, extracting mean and sd for polarisations

--- maskExtractHHHV.py
+++ maskExtractHHHV.py
@@ -14,9 +14,10 @@ import os
 mskFile = '/Volumes/ElementsSE/thesisData/validation/s2Mask/maskBool/aligned_s2Mask.tif'
 #for testing purposes
 fccPath = '/Volumes/ElementsSE/thesisData/FCCbatch_clipped/FCC_Sigma0_HHHV_20190412_clipped.tif'
+# directory containing clipped tifs
+directory = r'/Volumes/ElementsSE/thesisData/FCCbatch_clipped'
 
-
-def readSnwPrbMask(fp = mskFile):
+def readSnwPrbMask(fp = mskFile, threshold=60):
     # import and create binary snow mask
     snwPrbMask=rio.open(fp)
     snwPrbMask.meta
@@ -30,7 +31,7 @@ def readSnwPrbMask(fp = mskFile):
     # & for element-wise boolean-and
     #a_snwMaskBool[(~np.isnan(a_snwPrb)) & (a_snwPrb <= 60)] = 0.0
     #a_snwMaskBool[(~np.isnan(a_snwPrb)) & (a_snwPrb > 60)] = 1.0
-    mask_Zero = (~np.isnan(a_snwPrb)) & (a_snwPrb <= 60)
+    mask_Zero = (~np.isnan(a_snwPrb)) & (a_snwPrb <= threshold)
     mask_Nan = np.isnan(a_snwPrb)
     a_snwMaskBool = np.ones((a_snwPrb.shape[0],a_snwPrb.shape[1]),dtype=np.float32)
     #print(a_snwMaskBool)
@@ -52,6 +53,55 @@ def readSnwPrbMask(fp = mskFile):
 
 
 # next up --> extract bands from FCC to dict
+# read all files from directory
+
+def meanSdForTif(direc=directory):
+    # read boolean snow mask
+    _, snwBool = readSnwPrbMask()
+    hhMeanDict = {}
+    hhSdDict = {}
+    hvMeanDict = {}
+    hvSdDict = {}
+    
+    for entry in os.scandir(direc):
+        if entry.path.endswith("_clipped.tif") and entry.is_file():
+            # read fcc file
+            p = entry.path
+        else: continue
+    
+        splitDate = dateFromFilename(os.path.split(p)[-1])
+        print(splitDate)
+        hh, hv = readFcc(p) # original hh and hv with nan values
+            
+        # multiply glacier mask with polarisations
+        hh_masked = np.multiply(snwBool, hh)
+        hv_masked = np.multiply(snwBool, hv)
+        
+        #extract values for glaciarised areas
+        hh_msk_val = extractVals(hh_masked)
+        hv_msk_val = extractVals(hv_masked)
+        
+        # calculate mean and sd for glacierised areas
+        hhMean = hh_msk_val.mean()
+        hhSd = hh_msk_val.std()
+        print('Mean HH: ' + str(hhMean))
+        print('Standard dev HH: ' + str(hhSd))
+            
+        hvMean = hv_msk_val.mean()
+        hvSd = hv_msk_val.std()
+        print('Mean HH: ' + str(hvMean))
+        print('Standard dev HH: ' + str(hvSd))
+        
+        hhMeanDict[splitDate] = hhMean
+        hhSdDict[splitDate] = hhSd
+            
+        hvMeanDict[splitDate] = hvMean
+        hvSdDict[splitDate] = hvSd
+    
+    return hhMeanDict, hhSdDict, hvMeanDict, hvSdDict
+            
+
+
 
 def readFcc(fPath=fccPath):
     
@@ -67,7 +117,7 @@ def readFcc(fPath=fccPath):
     fcc_hh[fcc_hh == 0] = np.nan
     fcc_hv[fcc_hv == 0] = np.nan
     
-    plotPols(fcc_hh, fcc_hv, splitDate)
+    #plotPols(fcc_hh, fcc_hv, splitDate)
     
     printMinMax(fcc_hh, fcc_hv)
     
@@ -102,7 +152,12 @@ def printMinMax(hh, hv):
     print ('HV min: '+ str(hv.min()))
     print ('HV max: '+ str(hv.max()))
 
+
 ###### HELPER FUNCTIONS #####
+def extractVals(a):
+    a_Val = a[(~np.isnan(a)) & (a !=0)] # extract values that are neither nan or zero
+    return a_Val
+
 # extracting date from filename
 def dateFromFilename(fn):
     s1 = fn.split('_')[-2]
