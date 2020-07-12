4360165f543ff466075cb7f715c4a33f50d69e3e
gwib <gwib@users.noreply.github.com>
2020-07-12 14:34:11 +0200

    reading csv files properly, converting and merging columns

--- clusterCenters.py
+++ clusterCenters.py
@@ -7,11 +7,14 @@ Created on Fri Jul 10 16:18:19 2020
 
 import pandas as pd
 
+#windows
 inFolder = "C:/Users/galinavj/OneDrive - NTNU/thesisAnalysis/clustersFromMsk/clusterCSV/"
+#os
+inFolder = "/Volumes/ElementsSE/thesisData/toHist/mskClipped/clusterAftermath/clusterCSV/"
 
-clusterC_HH_f = inFolder+"Cluster Centers KMeans-HH.csv"
-clusterC_HV_f = inFolder+"Cluster Centers KMeans-HV.csv"
-clusterC_HHHV_f = inFolder+"Cluster Centers KMeans-HHHV.csv"
+clusterC_HH_f = inFolder+"ClusterCentersKMeans-HH.csv"
+clusterC_HV_f = inFolder+"ClusterCentersKMeans-HV.csv"
+clusterC_HHHV_f = inFolder+"ClusterCentersKMeans-HHHV.csv"
 
 ## FUNCTIONS
 def readDfFromFile(fp):
@@ -24,22 +27,37 @@ clusterHV_df = readDfFromFile(clusterC_HV_f)
 clusterHHHV_df = readDfFromFile(clusterC_HHHV_f)
 
 
-col_Names = ['Date','Cluster_1', 'Cluster_2','Cluster_3', 'Cluster_4', 'Cluster_5', 'Cluster_6']
+
 
 def clustersInCols(df,pol='HH'):
-    newDf = pd.DataFrame(columns=col_Names)
+    col_Names = ['Date','Cluster_1', 'Cluster_2','Cluster_3', 'Cluster_4', 'Cluster_5', 'Cluster_6']
+    colNames_pol = []
+    for c in col_Names:
+        if c=='Date':
+            colNames_pol.append(c)
+        elif 'Cluster' in  c:
+            colNames_pol.append(c+pol)
+            
+    #newDf = pd.DataFrame(columns=col_Names)
     l2 = []
     for d in set(list(df.Date)):
         l=[d]
         for c in [1,2,3,4,5,6]:
             l.append(df.loc[((df["Date"] == d) & (df.Cluster == c))][pol].iloc[0])
-        df_len = len(newDf)
-        print(df_len)
         l2.append(l)
-    newDf = pd.DataFrame(l2, columns=col_Names)
+    newDf = pd.DataFrame(l2, columns=colNames_pol)
         #df.loc[df_len] = l
         #l_df = pd.DataFrame([l], columns=newDf.columns)
         #print(l_df)
         #print(type(l_Series))
         #newDf.append(l_df)#, ignore_index=True)
     return newDf
+
+hhbyClusters = clustersInCols(clusterHH_df,'HH')
+hvbyClusters = clustersInCols(clusterHV_df,'HV')
+
+
+hhhvbyClusters_hh = clustersInCols(clusterHHHV_df,'HH')
+hhhvbyClusters_hv = clustersInCols(clusterHHHV_df, 'HV')
+
+hhhvbyClusters = pd.merge(hhhvbyClusters_hv, hhhvbyClusters_hh, on='Date')
